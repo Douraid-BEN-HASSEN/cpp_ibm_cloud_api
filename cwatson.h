b5f7fedebf00fe0d527f4057e0bc307389de7c1d
Douraïd BEN HASSEN <douraid.ben-hassen@mediatouch.fr>
2022-07-01 13:54:53 +0000

    initial commit

---
+++ cwatson.h
@@ -0,0 +1,26 @@
+#ifndef CWATSON_H
+#define CWATSON_H
+
+#include "cwebrequest.h"
+
+#include <QJsonObject>
+#include <QJsonDocument>
+
+// Classe utilitaire IBM
+
+class CWatson
+{
+public:
+    CWatson();
+    QByteArray getTTS(QString, QString, QString);
+    QString getSTT(QByteArray, QString);
+
+private:
+    QString _apiSTT_Key;
+    QString _apiSTT_Url;
+    QString _apiTTS_Key;
+    QString _apiTTS_Url;
+    CWebrequest *_request;
+};
+
+#endif // CWATSON_H
